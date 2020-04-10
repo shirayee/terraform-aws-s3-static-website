27b12d4ca186dd9d1fff7b866991ff27d20708fe
shirayee <16833735+shirayee@users.noreply.github.com>
2020-04-10 15:49:29 +0900

    add cloudfront distribution id

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "cdn_domain_name" {
   value       = "${aws_cloudfront_distribution.cdn.domain_name}"
 }
 output "s3_bucket_name" {
-  description = "name of s3 bucket"
+  description = "Name of s3 bucket"
   value = "${aws_s3_bucket.static_website.id}"
+}
+
+output "cloudfront_distribution_id" {
+  description = "ID of the Cloudfront Distribution"
+  value = "${aws_cloudfront_distribution.cdn.id}"
 }
